922222579173f4dab5e652c4cff005acbc805a53
Arthur Magalhães <oficialarthurmagalhaes@gmail.com>
2025-11-17 03:08:38 -0300

    adição de verificação de conexão banco

--- public/conexao.php
+++ public/conexao.php
@@ -7,4 +7,11 @@ $banco = 'teste_relatorios_php';
 
 $conexao = new mysqli($servidor,$usuario,$senha,$banco);
 
+// Verifica a conexão
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
+}
+
+// Define o charset para UTF-8 (evita problemas com acentuação)
+$conexao->set_charset("utf8");
 ?>
